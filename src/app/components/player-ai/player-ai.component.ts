e821a31b8f0536acadcedf96b3abcf28f239d355
Emerson Amorim <emerson_tecno@hotmail.com>
2023-10-11 15:53:18 -0300

    PlayCards Statemon - Angular com TypeScript 🚀 🔄 🌐

--- src/app/components/player-ai/player-ai.component.ts
+++ src/app/components/player-ai/player-ai.component.ts
@@ -9,41 +9,13 @@ import { MagicService } from 'src/app/services/magic.service';
 export class PlayerAIComponent implements OnInit {
 
   hand = [];
-  @Input() isOpponentsTurn: boolean;
-  @Input() deck: Object[];
-  @Input() topCard;
-  @Output() opponentDone = new EventEmitter();
-  @Output() opponentWon = new EventEmitter();
+
 
   constructor(private magics: MagicService) { }
 
   ngOnInit() {
     console.log('PlayerAIComponent Initialized');
   }
-
-
-  ngOnChanges() {
-    if (this.isOpponentsTurn && this.topCard && this.hand) {
-        this.magics.shuffleCards(this.hand);
-
-        const matchingCardIndex = this.hand.findIndex(card =>
-            card && (this.topCard.value === card.value || this.topCard.color === card.color)
-        );
-
-        if (matchingCardIndex !== -1) {
-            setTimeout(() => {
-                this.opponentDone.emit(this.hand.splice(matchingCardIndex, 1));
-            }, 1000);
-        } else {
-            this.hand.push(this.magics.drawCard(this.deck));
-        }
-
-        if (this.hand.length === 0) {
-            this.opponentWon.emit();
-        }
-    }
-}
-
 
 }
 
